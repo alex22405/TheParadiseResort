47e5a9557badf731e576a56b04020b3242b80e19
avigon <vigonalexandre@hotmail.com>
2023-07-16 23:00:47 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,72 @@
+const sr = ScrollReveal();
+
+sr.reveal('.main_title', {
+    origin: 'left',
+    distance: '50px',
+    duration: 3000,
+});
+
+sr.reveal('.main_description', {
+    origin: 'left',
+    distance: '50px',
+    duration: 3000,
+    delay: 500,
+});
+
+sr.reveal('.col50_title', {
+    origin: 'left',
+    distance: '50px',
+    duration: 3000,
+});
+
+sr.reveal('.col50_text', {
+    origin: 'left',
+    distance: '50px',
+    duration: 3000,
+});
+
+sr.reveal('.img_col50', {
+    origin: 'right',
+    distance: '50px',
+    duration: 3000,
+});
+
+sr.reveal('span', {
+    origin: 'top',
+    distance: '50px',
+    duration: 3000,
+});
+
+sr.reveal('.grid', {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 3000,
+    delay: 300
+});
+
+sr.reveal('.col_1', {
+    origin: 'left',
+    distance: '50px',
+    duration: 3000,
+    delay: 300
+});
+
+sr.reveal('.col_2', {
+    origin: 'right',
+    distance: '50px',
+    duration: 3000,
+    delay: 300
+});
+
+sr.reveal('.footer_box', {
+    origin: 'left',
+    distance: '50px',
+    duration: 3000,
+    delay: 300
+});
+
+sr.reveal('.col_30', {
+    origin: 'left',
+    distance: '50px',
+    duration: 3000,
+});
